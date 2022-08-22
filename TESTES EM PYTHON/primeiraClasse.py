00188eb7a6f475c40328472a614ff9b7b2f659cd
GetulioSSFilho <getuliosergio@hotmail.com>
2022-08-22 18:40:39 -0300

    teste inicial

---
+++ TESTES EM PYTHON/primeiraClasse.py
@@ -0,0 +1,8 @@
+class PrimeiraClasse:
+  def imprimir_mensagem(self, nome): 
+  #Criando um método
+    print(f"Olá {nome}, seja bem vindo!")
+
+
+objeto1 = PrimeiraClasse() 
+objeto1.imprimir_mensagem('João') 
