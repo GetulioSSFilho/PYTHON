90ab3795b7c71ced1dbf78e1b5fd5258ae4dcb31
GetulioSSFilho <getuliosergio@hotmail.com>
2022-08-22 18:43:04 -0300

    Passando Nome diferente

--- TESTES EM PYTHON/primeiraClasse.py
+++ TESTES EM PYTHON/primeiraClasse.py
@@ -1,8 +1,8 @@
 class PrimeiraClasse:
-  def imprimir_mensagem(self, nome): 
+  def imprimir_mensagem(self,nome): 
   #Criando um método
     print(f"Olá {nome}, seja bem vindo!")
 
 
 objeto1 = PrimeiraClasse() 
-objeto1.imprimir_mensagem('João') 
+objeto1.imprimir_mensagem('Getulio') 
